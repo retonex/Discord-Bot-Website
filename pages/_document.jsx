59d90e0d1b7a4194fe4244ce6b67763b9a143c73
DrWaygus <156544814+drwaygus@users.noreply.github.com>
2024-06-10 16:38:57 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,14 +11,14 @@ class MyDocument extends Document {
           <meta name="googlebot" content="index,follow" />
           <meta
             name="description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Sunucunuzu Nasıl Geliştireceğinizi ve aktif tutacağınızı mı düşünüyorsunuz? İşte Türkiyenin En kaltieli ve  Sistemli Botu Xeras Karşınızda!"
           />
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@Xeras" />
           <meta name="twitter:creator" content="@Xeras" />
           <meta property="og:url" content="https://Xeras.vercel.app" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Xeras" />
+          <meta property="og:title" content="Xeras Uyg" />
           <link
             rel="icon"
             href="/img/logo2.png"
@@ -26,14 +26,14 @@ class MyDocument extends Document {
           />
           <meta
             property="og:description"
-            content="How about listening to music without pausing? Create your own playlists with 750+ supported sound platforms and start listening."
+            content="Are you wondering how to improve your server and keep it active? Here is Turkey's most quality and systematic boat, Xeras!"
           />
           <meta property="og:image" content="/img/logo.jpg" />
           <meta property="og:image:alt" content="Xeras" />
           <meta property="og:locale" content="en_GB" />
           <meta
             property="og:site_name"
-            content="Xeras"
+            content="Xeras App"
           />
           <meta name="theme-color" content="#087ddd" />
           <link rel="icon" href="/img/logo.jpg" type="image/x-icon" />
