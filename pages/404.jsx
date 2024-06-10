535ae844374c400d44769c1acb088166c538dcb6
DrWaygus <156544814+drwaygus@users.noreply.github.com>
2024-06-10 08:13:14 +0300

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,9 +1,9 @@
 import ErrorPage from '../components/ErrorPage';
 const UnkownPage = () => {
     let messages = [
-        'Have you lost your way, kiddo?',
-        'Damn it! This dead-end road.',
-        'The Aurors blocked this road, Harry!'
+        'Yolunu mu kaybettin dostum?',
+        'Ah hayır, Burası çıkmaz yol!.',
+        'Girişi ve çıkışı olmayana yönlendirilidiniz'
     ];
 
     return <ErrorPage code={404} message={messages[Math.floor(Math.random()*messages.length)] || "Page not found."} />
