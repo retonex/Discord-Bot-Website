578dabc31de3af2b219c4c7acf9756f5962e5ce2
Retonex <156544814+retonex@users.noreply.github.com>
2024-08-15 22:37:20 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -57,7 +57,7 @@ export default function AwardApp({ Component, pageProps }) {
       />
       <Head>
         <title>
-          Xeras
+          Wexam
         </title>
       </Head>
       <main className="transition-all duration-200 z-10 absolute inset-0 px-5 h-screen max-w-7xl w-full mx-auto">
